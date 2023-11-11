265fe58ca6da6159127272e1d67115aa30178ac3
Alena Katkova <katkova.web@gmail.com>
2023-11-11 22:37:33 +0100

    add search bar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,26 +1,112 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { NavLink } from "react-router-dom";
+
+const AAD_NAV_LINK_STYLES = {
+  display: "inline-block",
+  fontStyle: "normal",
+  fontWeight: 500,
+  fontSize: "18px",
+  lineHeight: "20px",
+  color: "#000000",
+  marginBottom: "10px",
+};
+
+const NAV_LINK_STYLES = {
+  display: "inline-block",
+  fontStyle: "normal",
+  fontWeight: 400,
+  fontSize: "18px",
+  lineHeight: "20px",
+  color: "#000000",
+  marginBottom: "10px",
+};
+
+const PLUS_STYLES = {
+  display: "inline-block",
+  marginRight: "10px",
+  fontStyle: "normal",
+  fontWeight: 600,
+  fontSize: "24px",
+  color: "#000000",
+};
 
 const Header = () => {
   return (
     <Box
-      component="nav"
       sx={{
-        display: "flex",
-        flexDirection: "column",
-        padding: "0.5rem",
-        gap: 2,
+        padding: "70px 40px",
+        backgroundColor: "#F8F8FB",
+        height: "100vh",
       }}
     >
-      <NavLink to="/signin">Sign In</NavLink>
-      <NavLink to="/signup">Sign Up</NavLink>
-      <NavLink to="/dashboard">Dashboard</NavLink>
-      <NavLink to="/dashboard/add-company">Add Company</NavLink>
-      <NavLink to="/dashboard/company-details">Company Details</NavLink>
-      <NavLink to="/dashboard/set-goals">Set Goals</NavLink>
-      <NavLink to="/dashboard/goals">Goals</NavLink>
-      <NavLink to="/dashboard/add-event">Add Event</NavLink>
+      <Box
+        component="nav"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          padding: "0.5rem",
+          gap: 2,
+        }}
+      >
+        <Box
+          sx={{
+            boxSizing: "border-box",
+            width: "100%",
+            padding: "20px",
+            backgroundColor: "gray",
+            marginBottom: "40px",
+            textAlign: "center",
+          }}
+        >
+          LOGO
+        </Box>
+        <NavLink to="/dashboard/add-company">
+          <Typography component="span" sx={PLUS_STYLES}>
+            +
+          </Typography>
+          <Typography component="span" sx={AAD_NAV_LINK_STYLES}>
+            Add Company
+          </Typography>
+        </NavLink>
+        <NavLink to="/dashboard/set-goals">
+          <Typography component="span" sx={PLUS_STYLES}>
+            +
+          </Typography>
+          <Typography component="span" sx={AAD_NAV_LINK_STYLES}>
+            Set Goals
+          </Typography>
+        </NavLink>
+        <NavLink to="/dashboard/add-event">
+          <Typography component="span" component="span" sx={PLUS_STYLES}>
+            +
+          </Typography>
+          <Typography component="span" sx={AAD_NAV_LINK_STYLES}>
+            Add Event
+          </Typography>
+        </NavLink>
+        <NavLink to="/dashboard">
+          <Typography component="span" sx={NAV_LINK_STYLES}>
+            Dashboard
+          </Typography>
+        </NavLink>
+        <NavLink to="/dashboard/company-details">
+          <Typography component="span" sx={NAV_LINK_STYLES}>
+            Company Details
+          </Typography>
+        </NavLink>
+        <NavLink to="/dashboard/goals">
+          <Typography component="span" sx={NAV_LINK_STYLES}>
+            Goals
+          </Typography>
+        </NavLink>
+
+        <NavLink to="/dashboard/events">
+          <Typography component="span" sx={NAV_LINK_STYLES}>
+            Events
+          </Typography>
+        </NavLink>
+      </Box>
     </Box>
   );
 };
