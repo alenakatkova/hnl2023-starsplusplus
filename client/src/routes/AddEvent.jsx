7aef3a4610ea4de7315ed815424c53c77369271f
Alena Katkova <katkova.web@gmail.com>
2023-11-11 23:10:01 +0100

    styles for add event

--- client/src/routes/AddEvent.jsx
+++ client/src/routes/AddEvent.jsx
@@ -8,8 +8,11 @@ import {
   TextField,
   Chip,
   OutlinedInput,
+  Typography,
+  Button,
 } from "@mui/material";
 import { useLoaderData } from "react-router-dom";
+import { CARD_STYLES } from "../style-constants.js";
 
 const getMetricsWithArea = (ngo) => {
   return ngo.impactAreas.flatMap((impactArea) =>
@@ -46,65 +49,118 @@ function AddEvent() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Submit logic here
     console.log({ eventName, eventDates, location, selectedMetrics });
   };
 
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit}
-      sx={{ width: "100%", "& .MuiTextField-root": { m: 1 } }}
-    >
-      <TextField
-        label="Event Name"
-        fullWidth
-        value={eventName}
-        onChange={(e) => setEventName(e.target.value)}
-      />
+    <Box>
+      <Typography variant="h1">Create Event</Typography>
+      <Box
+        component="form"
+        sx={{
+          ...CARD_STYLES,
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+          width: "50%",
+        }}
+        onSubmit={handleSubmit}
+      >
+        <FormControl fullWidth>
+          <Typography
+            component="label"
+            sx={{
+              fontSize: "18px",
+              fontWeight: 400,
+              marginBottom: "15px",
+            }}
+          >
+            Event Name
+          </Typography>
+          <TextField
+            fullWidth
+            value={eventName}
+            onChange={(e) => setEventName(e.target.value)}
+          />
+        </FormControl>
 
-      <TextField
-        label="Event Dates"
-        fullWidth
-        value={eventDates}
-        onChange={(e) => setEventDates(e.target.value)}
-      />
+        <FormControl fullWidth>
+          <Typography
+            component="label"
+            sx={{
+              fontSize: "18px",
+              fontWeight: 400,
+              marginBottom: "15px",
+            }}
+          >
+            Event Dates
+          </Typography>
+          <TextField
+            fullWidth
+            value={eventDates}
+            onChange={(e) => setEventDates(e.target.value)}
+          />
+        </FormControl>
 
-      <TextField
-        label="Location"
-        fullWidth
-        value={location}
-        onChange={(e) => setLocation(e.target.value)}
-      />
+        <FormControl fullWidth>
+          <Typography
+            component="label"
+            sx={{
+              fontSize: "18px",
+              fontWeight: 400,
+              marginBottom: "15px",
+            }}
+          >
+            Location
+          </Typography>
+          <TextField
+            fullWidth
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </FormControl>
 
-      <FormControl fullWidth sx={{ m: 1 }}>
-        <InputLabel>Impact Metrics</InputLabel>
-        <Select
-          multiple
-          value={selectedMetrics}
-          onChange={handleMetricChange}
-          input={<OutlinedInput label="Impact Metrics" />}
-          renderValue={(selected) => (
-            <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-              {selected.map((value) => (
-                <Chip
-                  key={value}
-                  label={value}
-                  onDelete={() => handleDeleteMetric(value)}
-                />
-              ))}
-            </Box>
-          )}
-        >
-          {metrics.map((metric) => (
-            <MenuItem key={metric.id} value={metric.name}>
-              {metric.name} ({metric.area})
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+        <FormControl fullWidth sx={{ my: 1 }}>
+          <Typography
+            component="label"
+            sx={{
+              fontSize: "18px",
+              fontWeight: 400,
+              marginBottom: "15px",
+            }}
+          >
+            Impact Metrics
+          </Typography>
+          <Select
+            sx={{ backgroundColor: "white" }}
+            multiple
+            value={selectedMetrics}
+            onChange={handleMetricChange}
+            input={<OutlinedInput label="Impact Metrics" />}
+            renderValue={(selected) => (
+              <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                {selected.map((value) => (
+                  <Chip
+                    key={value}
+                    label={value}
+                    onDelete={() => handleDeleteMetric(value)}
+                  />
+                ))}
+              </Box>
+            )}
+          >
+            {metrics.map((metric) => (
+              <MenuItem key={metric.id} value={metric.name}>
+                {metric.name} ({metric.area})
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
 
-      <button type="submit">Add Event</button>
+        <Button type="submit" sx={{ color: "white" }}>
+          Add Event
+        </Button>
+      </Box>
     </Box>
   );
 }
