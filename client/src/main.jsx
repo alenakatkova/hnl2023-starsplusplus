7e9691b7113a2c16b070e0e37fbe940a6abb9ae7
Alena Katkova <katkova.web@gmail.com>
2023-11-11 22:11:14 +0100

    styles for set goal page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,57 @@ import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const router = createBrowserRouter(routes);
 
+const theme = createTheme({
+  typography: {
+    h1: {
+      fontSize: "36px",
+      fontWeight: 500,
+      color: "black",
+      marginBottom: "40px",
+    },
+  },
+  components: {
+    // Name of the component
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "white",
+        },
+      },
+    },
+    MuiSelect: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "white",
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        // Name of the slot
+        root: {
+          backgroundColor: "#6C798C",
+          textTransform: "none",
+          paddingBottom: "21px",
+          paddingTop: "21px",
+          border: "none",
+          boxShadow: "none",
+          minWidth: "60%",
+          fontSize: "18px",
+        },
+      },
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 );
---
+++ client/src/style-constants.js
@@ -0,0 +1,5 @@
+export const FORM_STYLES = {
+  backgroundColor: "#F6F6F6",
+  padding: "48px",
+  borderRadius: "30px",
+};
